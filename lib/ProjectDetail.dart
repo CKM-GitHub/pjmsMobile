22e52c2053009c81a1ac2de3ec28ecc3ef0c5e86
Kyaw Thet Paing <capital.kyawthetpaing@gmail.com>
2021-08-06 10:30:16 +0630

    ktp

--- lib/ProjectDetail.dart
+++ lib/ProjectDetail.dart
@@ -3,38 +3,67 @@ import 'package:http/http.dart';
 import 'package:flutter/material.dart';
 
 class ProjectDetail extends StatefulWidget {
-  final String detailCD = '';
-  ProjectDetail(detailCD);
+  final String detailCD;
+  ProjectDetail(this.detailCD);
+
   @override
   _ProjectDetailPageState createState() => _ProjectDetailPageState();
 }
 
 class _ProjectDetailPageState extends State<ProjectDetail> {
-  String projectCD = '';
+  /*String _projectCD = '';
+  String _projectName = '';
+  String _projectTypeName = '';
+  String _companyName = '';
+  String _personInCharege = '';
+  String _relatedCompanyName = '';*/
+
+  // ignore: unused_element
+  Future<Project> _getProjects() async {
+    String username = 'KTP';
+    String password = 'KTP12345!';
+    String basicAuth =
+        'Basic ' + base64Encode(utf8.encode('$username:$password'));
+    print(basicAuth);
+
+    Response r = await post(
+        Uri.parse('http://163.43.113.92/PJMS/api/ProjectListApi/GetProject'),
+        headers: <String, String>{
+          'authorization': basicAuth
+        },
+        body: <String, String>{
+          'ProjectCD': widget.detailCD,
+        });
+
+    dynamic jsonData = jsonDecode(jsonDecode(r.body));
+
+    Project p = new Project(
+        jsonData[0]['ProjectCD'],
+        jsonData[0]['ProjectName'],
+        jsonData[0]['ProjectTypeName'],
+        jsonData[0]['CompanyName'],
+        jsonData[0]['PersonInCharge'],
+        jsonData[0]['RelatedCompanyName']);
+
+    return p;
+    /*_projectCD = jsonData[0]['ProjectCD'];
+    _projectName = jsonData[0]['ProjectName'];
+    _projectName = jsonData[0]['ProjectTypeName'] == null
+        ? ''
+        : jsonData[0]['ProjectTypeName'];
+    _companyName =
+        jsonData[0]['CompanyName'] == null ? '' : jsonData[0]['CompanyName'];
+    _personInCharege = jsonData[0]['PersonInCharge'] == null
+        ? ''
+        : jsonData[0]['PersonInCharge'];
+    _relatedCompanyName = jsonData[0]['RelatedCompanyName'] == null
+        ? ''
+        : jsonData[0]['RelatedCompanyName'];
+        */
+  }
 
   @override
   Widget build(BuildContext context) {
-    Future<dynamic> _getProjects() async {
-      String username = 'KTP';
-      String password = 'KTP12345!';
-      String basicAuth =
-          'Basic ' + base64Encode(utf8.encode('$username:$password'));
-      print(basicAuth);
-
-      Response r = await post(
-          Uri.parse('http://163.43.113.92/PJMS/api/ProjectListApi/GetProject'),
-          headers: <String, String>{
-            'authorization': basicAuth
-          },
-          body: <String, String>{
-            'ProjectCD': widget.detailCD,
-          });
-
-      dynamic jsonData = jsonDecode(jsonDecode(r.body));
-      projectCD = jsonData[0]['ProjectCD'];
-      return jsonData;
-    }
-
     return Scaffold(
         appBar: AppBar(
           leading: IconButton(
@@ -44,48 +73,74 @@ class _ProjectDetailPageState extends State<ProjectDetail> {
           backgroundColor: Color.fromRGBO(143, 148, 251, 1),
           title: Text("プロジェクト管理"),
         ),
-        body: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.only(top: 5, bottom: 0.5, left: 3, right: 3),
-              padding: EdgeInsets.only(right: 5),
-              child: Column(
-                children: [
-                  W1('プロジェクトCD', projectCD),
-                  Divider(
-                    color: Color.fromRGBO(143, 148, 251, 1),
-                  ),
-                  W1('プロジェクト名', ''),
-                  Divider(
-                    color: Color.fromRGBO(143, 148, 251, 1),
-                  ),
-                  W1('会社名（お客さん）', ''),
-                  Divider(
-                    color: Color.fromRGBO(143, 148, 251, 1),
-                  ),
-                  W1('担当者（お客さん）', ''),
-                  Divider(
-                    color: Color.fromRGBO(143, 148, 251, 1),
-                  ),
-                  W1('プロジェクト区分', ''),
-                  Divider(
-                    color: Color.fromRGBO(143, 148, 251, 1),
-                  ),
-                  W1('関係する会社名', ''),
-                ],
-              ),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                      color: Color.fromRGBO(143, 148, 251, 1), spreadRadius: 1),
-                ],
-              ),
-              height: 381,
-            ),
-          ],
-        ));
+        body: FutureBuilder(
+            future: _getProjects(),
+            builder: (context, snapshot) {
+              Project p;
+              if (snapshot.hasData) {
+                p = snapshot.data as Project;
+              } else {
+                p = new Project('', '', '', '', '', '');
+              }
+              return snapshot.hasData
+                  ? Column(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(
+                              top: 5, bottom: 0.5, left: 3, right: 3),
+                          padding: EdgeInsets.only(right: 5),
+                          child: Column(
+                            children: [
+                              W1('プロジェクトCD', p.projectCD),
+                              Divider(
+                                color: Color.fromRGBO(143, 148, 251, 1),
+                              ),
+                              W1('プロジェクト名', p.projectName),
+                              Divider(
+                                color: Color.fromRGBO(143, 148, 251, 1),
+                              ),
+                              W1('会社名（お客さん）', p.companyName),
+                              Divider(
+                                color: Color.fromRGBO(143, 148, 251, 1),
+                              ),
+                              W1('担当者（お客さん）', p.personInCharge),
+                              Divider(
+                                color: Color.fromRGBO(143, 148, 251, 1),
+                              ),
+                              W1('プロジェクト区分', p.projectTypeName),
+                              Divider(
+                                color: Color.fromRGBO(143, 148, 251, 1),
+                              ),
+                              W1('関係する会社名', p.relatedCompany),
+                            ],
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Color.fromRGBO(143, 148, 251, 1),
+                                  spreadRadius: 1),
+                            ],
+                          ),
+                          height: 381,
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(
+                              top: 5, bottom: 0.5, left: 3, right: 3),
+                          padding: EdgeInsets.only(right: 5),
+                          child: Column(
+                            children: [
+                              
+                            ]
+                          )
+                        ),
+                      ],
+                    )
+                  : Center(
+                      child: Text("There is no Data"),
+                    );
+            }));
   }
 }
 
